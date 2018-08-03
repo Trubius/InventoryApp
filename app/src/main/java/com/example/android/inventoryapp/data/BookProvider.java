84f432219a3a3d6658e61833de844415c70938ac
Trubius <trubel.gabor@gmail.com>
2018-08-03 13:02:58 +0100

    Add UriMatcher to BookProvider class

--- app/src/main/java/com/example/android/inventoryapp/data/BookProvider.java
+++ app/src/main/java/com/example/android/inventoryapp/data/BookProvider.java
@@ -2,12 +2,22 @@ package com.example.android.inventoryapp.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 public class BookProvider extends ContentProvider {
+
+    private static final int BOOKS = 1000;
+    private static final int BOOK_ID = 1001;
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        sUriMatcher.addURI(BookContract.CONTENT_AUTHORITY, BookContract.PATH_BOOKS, BOOKS);
+        sUriMatcher.addURI(BookContract.CONTENT_AUTHORITY, BookContract.PATH_BOOKS + "/#", BOOK_ID);
+    }
 
     private BookDbHelper mBookDbHelper;
 
